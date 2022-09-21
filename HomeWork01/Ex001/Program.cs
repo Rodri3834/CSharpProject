eca7f9aa334af39eb7085b610936534ef0008a17
Vlad <vladislav.klimenok@gmail.com>
2022-09-21 12:34:20 +0300

    Сделал задание 1

---
+++ HomeWork01/Ex001/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = -9;
+int b = -3;
+int max;
+int min;
+if(a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.Write("Максимальное число - ");
+Console.WriteLine(max);
+Console.Write("Минимальное число - ");
+Console.WriteLine(min);
