2d9f4223fb6621b553cc4065abe59ba410f3d57a
Vlad <vladislav.klimenok@gmail.com>
2022-09-26 14:11:36 +0300

    Сделал задачу №228 в домашнем задании.

---
+++ HomeWork02/Ex004/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 228: Напишите программу, которая принимает на вход семь чисел, и находит их среднее арифметическое
+//1 2 3 4 5 6 7-> 4
+//10 20 30 40 50 60 70 -> 40
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 4: ");
+int number4 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 5: ");
+int number5 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 6: ");
+int number6 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 7: ");
+int number7 = Convert.ToInt32(Console.ReadLine());
+
+int SrArf = (number1 +  number2 + number3 + number4 + number5 + number6 + number7)/7;
+
+Console.WriteLine("Среднее арифметическое этих чисел равно " + SrArf);
