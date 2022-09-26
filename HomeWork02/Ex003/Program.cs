52dc211a5c26b98450cafa18b30c47a7f27b13fa
Vlad <vladislav.klimenok@gmail.com>
2022-09-26 13:36:33 +0300

    Сделал задачу №15 в домашнем задании.

---
+++ HomeWork02/Ex003/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number <8)
+{
+    if (number > 5)
+    {
+        Console.WriteLine("Этот день - ВЫХОДНОЙ!!!");
+    }
+    else
+    {
+        Console.WriteLine("Это будний день.");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели неправильный день недели.");
+}
