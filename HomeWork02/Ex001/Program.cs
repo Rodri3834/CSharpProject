388e09657d39dfcc1f50088feed7de6ebe89e6c1
Vlad <vladislav.klimenok@gmail.com>
2022-09-26 13:18:30 +0300

    Сделал задачу №10 в домашнем задании.

---
+++ HomeWork02/Ex001/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.Write("Введи трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    string Strnumber = Convert.ToString(number);
+    Console.WriteLine("вторая цифра этого числа это - " + Strnumber[1]);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");
+}
